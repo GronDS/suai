bcaa28027d27e8b53917d99fe1a9ce7045e9922e
GronDS <gronsokolov@gmail.com>
2023-12-03 03:19:32 +0300

    . 	modified:   module_2/practice_2/figures/ellipse.py 	new file:   module_2/practice_2/figures/quads.py

--- module_2/practice_2/figures/ellipse.py
+++ module_2/practice_2/figures/ellipse.py
@@ -7,6 +7,6 @@ class Ellipse(Oval):
         return super().draw(x, y)
 
 if __name__ == '__main__':
-    ellipse_1 = Ellipse(1, 2, 'red', 'yellow', 2, 100, 70)
-    ellipse_1.draw(ellipse_1._pos_x, ellipse_1._pos_y)
+    ellipse_1 = Ellipse('red', 'yellow', 2, 100, 70)
+    ellipse_1.draw(1, 2)
     done()
---
+++ module_2/practice_2/figures/quads.py
@@ -0,0 +1,7 @@
+from figure import Figure
+
+
+class Quadrilaterals(Figure):
+    
+    def draw(self):
+        return super().draw()
